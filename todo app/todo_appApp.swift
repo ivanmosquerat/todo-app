177f3973361036cf8df2cdeeb61621d6321f7ac3
Ivan Mosquera <ivanmosquerat13@gmail.com>
2021-02-27 00:00:17 +1100

    Initial Commit

---
+++ todo app/todo_appApp.swift
@@ -0,0 +1,20 @@
+//
+//  todo_appApp.swift
+//  todo app
+//
+//  Created by Ivan Mosquera on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
