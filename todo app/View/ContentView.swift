d09946411c6044d09bad5747b8a3d1aa9418478c
Ivan Mosquera <ivanmosquerat13@gmail.com>
2021-02-28 17:58:15 +1100

    Image and text for empty view on list created.

--- todo app/View/ContentView.swift
+++ todo app/View/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     // MARK: - Properties
     @Environment(\.managedObjectContext) private var viewContext
-    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
+    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var tasks: FetchedResults<Todo>
     @State private var showingAddTodoView: Bool = false
 
     // MARK: - Body
@@ -20,31 +20,58 @@ struct ContentView: View {
         
         NavigationView{
             
-            List{
-                ForEach(self.todos, id: \.self){ todo in
-                    HStack{
-                        Text(todo.name ?? "Unknown")
-                        
-                        Spacer()
-                        
-                        Text(todo.priority ?? "Undefined")
-                    }
-                }//: Loop
-            }//: List
-            .listStyle(InsetListStyle())
-            .navigationBarTitle("Task", displayMode: .inline)
-            .navigationBarItems(trailing: Button(action: {
-                self.showingAddTodoView.toggle()
-            }, label: {
-                Image(systemName: "plus")
-            }))
-            .sheet(isPresented: $showingAddTodoView, content: {
-                AddTodoView()
-                    .environment(\.managedObjectContext, self.viewContext)
-            })
+            ZStack {
+                List{
+                    ForEach(self.tasks, id: \.self){ todo in
+                        HStack{
+                            Text(todo.name ?? "Unknown")
+                            
+                            Spacer()
+                            
+                            Text(todo.priority ?? "Undefined")
+                        }
+                    }//: Loop
+                    .onDelete(perform: deleteTask)
+                }//: List
+                .listStyle(InsetListStyle())
+                .navigationBarTitle("Task", displayMode: .inline)
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button(action: {
+                    self.showingAddTodoView.toggle()
+                }, label: {
+                    Image(systemName: "plus")
+                }))
+                .sheet(isPresented: $showingAddTodoView, content: {
+                    AddTodoView()
+                        .environment(\.managedObjectContext, self.viewContext)
+                })
+                
+                // MARK: - No task items
+                if tasks.count == 0 {
+                    EmptyListView()
+                }
+            }//: ZStack
             
         }//: Navigation
-        
+    }
+    
+    // MARK: - Functions
+    
+    
+    /// <#Description#>
+    /// - Parameter offsets: <#offsets description#>
+    private func deleteTask(at offsets: IndexSet){
+        for index in offsets{
+            let task = tasks[index]
+            viewContext.delete(task)
+            
+            do{
+                try viewContext.save()
+            }catch{
+                print(error)
+            }
+        }
     }
 
 }
